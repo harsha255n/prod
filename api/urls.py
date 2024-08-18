32706cc67506824cb74a3c0950e39349c504fc86
harshan <mohammedharshan255@gmail.com>
2024-08-18 11:09:10 +0530

    new added

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,9 @@ from api import views
 
 urlpatterns = [
     path("create", views.CreateProductView.as_view()),
-   
+    path("list", views.ListProductsView.as_view()),
+    path("add", views.AddStockView.as_view()),
+    path("add", views.DeleteProductView.as_view()),
      
 
 ]
--- api/views.py
+++ api/views.py
@@ -19,3 +19,34 @@ class CreateProductView(APIView):
                             subvariant_serializer.save(variant=variant_serializer.instance)
             return Response({"message": "Product created successfully"}, status=201)
         return Response({"error": "Invalid request"}, status=400)
+    
+
+class ListProductsView(APIView):
+     def get(self, request):
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class AddStockView(APIView):
+    def post(self, request):
+        data = request.data
+        product_variant_id = data.get("product_variant_id")
+        quantity = data.get("quantity")
+        if product_variant_id and quantity:
+            try:
+                product_variant = Variants.objects.get(id=product_variant_id)
+                stock = Stock(product_variant=product_variant, quantity=quantity)
+                stock.save()
+                return Response({"message": "Stock added successfully"}, status=201)
+            except Variants.DoesNotExist:
+                return Response({"error": "Product variant not found"}, status=404)
+        return Response({"error": "Invalid request"}, status=400)
+
+class DeleteProductView(APIView):
+    def delete(self, request, product_id):
+        try:
+            product = Products.objects.get(id=product_id)
+            product.delete()
+            return Response({"message": "Product deleted successfully"}, status=200)
+        except Products.DoesNotExist:
+            return Response({"error": "Product not found"}, status=404)
